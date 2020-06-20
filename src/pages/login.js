127c9d7e7e9dde4ae54ecd66898c47ea3fac8371
Anqi Chen <an883236@dal.ca>
2020-06-20 15:54:12 -0300

    login.js is updated for better responsiveness

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "antd/dist/antd.css";
-import { Card, Input, Button, Spin } from "antd";
+import { Card, Input, Button, Spin, Col, Row } from "antd";
 import { UserOutlined, KeyOutlined } from "@ant-design/icons";
 import "../style/login.css";
 
@@ -17,40 +17,44 @@ function Login() {
   };
 
   return (
-    <div className="login-div">
+    <div>
       <Spin tip="Loading..." spinning={isLoading}>
-        <Card
-          title="Sign in to Noval Life"
-          bordered={true}
-          style={{ width: 400 }}
-        >
-          <Input
-            id="userName"
-            size="large"
-            placeholder="Enter your userName"
-            prefix={<UserOutlined />}
-            onChange={(e) => {
-              setUserName(e.target.value);
-            }}
-          />
-          <br />
-          <br />
-          <Input.Password
-            id="password"
-            size="large"
-            placeholder="Enter your password"
-            prefix={<KeyOutlined />}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-          <br />
-          <br />
-          <Button type="primary" size="large" block onClick={checkLogin}>
-            Sign in
-          </Button>
-          Or <a href="/signup">register now!</a>
-        </Card>
+        <Row justify="center">
+          <Col xs={20} sm={16} md={12} lg={8} xl={6}>
+            <Card
+              title="Sign in to Noval Life"
+              bordered={true}
+              style={{ width: "100%" }}
+            >
+              <Input
+                id="userName"
+                size="large"
+                placeholder="Enter your userName"
+                prefix={<UserOutlined />}
+                onChange={(e) => {
+                  setUserName(e.target.value);
+                }}
+              />
+              <br />
+              <br />
+              <Input.Password
+                id="password"
+                size="large"
+                placeholder="Enter your password"
+                prefix={<KeyOutlined />}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              <br />
+              <br />
+              <Button type="primary" size="large" block onClick={checkLogin}>
+                Sign in
+              </Button>
+              Or <a href="/signup">register now!</a>
+            </Card>
+          </Col>
+        </Row>
       </Spin>
     </div>
   );
