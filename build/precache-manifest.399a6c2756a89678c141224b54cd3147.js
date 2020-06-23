c1c75e8998f7466507a7c74c5c82fb8e194ab29d
Shivam Gupta <shivam.gupta@dal.ca>
2020-06-23 19:27:28 -0300

    build created

---
+++ build/precache-manifest.399a6c2756a89678c141224b54cd3147.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa801732cdfb3d71a1947b6f8827aa63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9f01147f122aa98ee46",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "606a62b3da03894514e6",
+    "url": "/static/css/main.e7c057db.chunk.css"
+  },
+  {
+    "revision": "a9f01147f122aa98ee46",
+    "url": "/static/js/2.eca76769.chunk.js"
+  },
+  {
+    "revision": "5a26a773196b8cf79c823d99fda61703",
+    "url": "/static/js/2.eca76769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606a62b3da03894514e6",
+    "url": "/static/js/main.164daebe.chunk.js"
+  },
+  {
+    "revision": "edaa41145e006b37bed0",
+    "url": "/static/js/runtime-main.02fb610d.js"
+  }
+]);
