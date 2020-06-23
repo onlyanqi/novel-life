c168441d0a4ded2e87fa38e96eda49e8dd4b4b27
sashutosh <B00836612>
2020-06-22 23:25:00 -0300

    symptoms

---
+++ src/pages/SymptomChecker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Symptoms from '../pages/SymptomChecker/Symptom.js'
+
+
+const Symptom = (props) => {
+    return (
+        <div>
+            <Symptoms/>
+           
+            </div>   
+
+    )};
+
+export default Symptom;
